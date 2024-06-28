f86b112ad2f36e3ead5014cfe5e07eec8e0a43ae
Diipam <dhakaldiipam@gmail.com>
2024-06-28 12:30:00 +0545

    Initial Commit

---
+++ WhatsApp Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsApp Clone
+//
+//  Created by Smart Solar Nepal on 28/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsApp Clone/WhatsApp_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsApp_CloneApp.swift
+//  WhatsApp Clone
+//
+//  Created by Smart Solar Nepal on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsApp_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
